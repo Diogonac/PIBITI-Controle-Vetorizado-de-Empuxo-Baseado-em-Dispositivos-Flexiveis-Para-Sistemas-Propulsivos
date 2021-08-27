823042d1ba3eb2acc3423104e1e18950b50ac153
Diogonac <diogonac@al.insper.edu.br>
2021-08-27 07:35:21 -0300

    testes

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 //#include "programs/teste_mixer.h"
 
-//#include "programs/teste_controlador_atitude.h"
+#include "programs/teste_controlador_atitude.h"
 
 //#include "programs/teste_kalman.h"
 
-#include "programs/calibra_angulos_BNO_MPU.h"
+//#include "programs/calibra_angulos_BNO_MPU.h"
