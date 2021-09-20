2108a0856e2b2f7ef5096f49a31acaf3cbdf0aef
Diogonac <diogonac@al.insper.edu.br>
2021-09-20 14:51:21 -0300

    add

--- src/utils/pin_names.h
+++ src/utils/pin_names.h
@@ -28,9 +28,9 @@ const PinName SERIAL_TX1 = D1;
 const PinName SERIAL_RX1 = D0;
 
 // Pinos dos indicadores
-const PinName AMARELO = D7; //Mudar para D8
-const PinName VERDE = D8; //Mudar para D9
-const PinName VERMELHO = D9; //Mudar para D10
-const PinName BUZZER = D10; //Mudar para D11
+const PinName AMARELO = D8; //Mudar para D8
+const PinName VERDE = D9; //Mudar para D9
+const PinName VERMELHO = D10; //Mudar para D10
+const PinName BUZZER = D11; //Mudar para D11
 
 #endif
